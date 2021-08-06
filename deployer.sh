be3ac0375896d7eeda202f35fe0823d8d48b70c4
0xI2C <88055104+0xI2C@users.noreply.github.com>
2021-08-06 18:29:13 +0000

    Some wired fix's

--- deployer.sh
+++ deployer.sh
@@ -1,16 +1,16 @@
 sudo adduser $(whoami) kvm
-exec /bin/bash -l
+newgrp kvm
 wget https://github.com/0xI2C/resources-required/raw/main/as-provider.sh
 wget https://github.com/0xI2C/resources-required/raw/main/golemsp.service
 sudo chmod +x as-provider.sh
 sudo chmod +x golemsp.service
-./as-provider.sh
-echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
-export PATH="$HOME/.local/bin:$PATH"
-golemsp settings set --node-name $(date +%s)
-golemsp settings set --starting-fee 0
-golemsp settings set --env-per-hour 0.0
-golemsp settings set --cpu-per-hour 0.015
-golemsp settings set --account 0xc018A306Ab457e2aB37FEA9AEAa06237f1B00476
-echo "0.0015" | nohup golemsp run >/dev/null 2>&1 &
 sudo systemctl enable golemsp.service
+./as-provider.sh
+./$HOME/.local/bin/golemsp settings set --node-name $(date +%s)
+./$HOME/.local/bin/golemsp settings set --starting-fee 0
+./$HOME/.local/bin/golemsp settings set --env-per-hour 0.0
+./$HOME/.local/bin/golemsp settings set --cpu-per-hour 0.015
+./$HOME/.local/bin/golemsp settings set --account 0xc018A306Ab457e2aB37FEA9AEAa06237f1B00476
+echo "0.0015" | nohup ./$HOME/.local/bin/golemsp run >/dev/null 2>&1 &
+rm golemsp.service
+rm as-provider.sh
