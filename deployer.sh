3f6894dc9810c01afa657f467fc5c93b747152f9
0xI2C <88055104+0xI2C@users.noreply.github.com>
2021-08-06 03:39:47 +0000

    Fix permissions

--- deployer.sh
+++ deployer.sh
@@ -1,8 +1,8 @@
-adduser $(whoami) kvm
+sudo adduser $(whoami) kvm
 wget https://github.com/0xI2C/resources-required/raw/main/as-provider.sh
 wget https://github.com/0xI2C/resources-required/raw/main/golemsp.service
-chmod +x as-provider.sh
-chmod +x golemsp.service
+sudo chmod +x as-provider.sh
+sudo chmod +x golemsp.service
 ./as-provider.sh
 echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
 export PATH="$HOME/.local/bin:$PATH"
@@ -11,5 +11,5 @@ golemsp settings set --starting-fee 0
 golemsp settings set --env-per-hour 0.01
 golemsp settings set --cpu-per-hour 0.016
 golemsp settings set --account 0xc018A306Ab457e2aB37FEA9AEAa06237f1B00476
-systemctl enable golemsp.service
-systemctl start golemsp.service
+sudo systemctl enable golemsp.service
+sudo systemctl start golemsp.service
