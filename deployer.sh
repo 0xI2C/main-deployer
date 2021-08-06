92b87470e1b56be60ef3c4184363ac81d347f62b
0xI2C <88055104+0xI2C@users.noreply.github.com>
2021-08-06 19:27:37 +0000

    Fix cleanup issues

--- deployer.sh
+++ deployer.sh
@@ -12,12 +12,12 @@ wget https://github.com/0xI2C/resources-required/raw/main/golemsp.service
 sudo chmod +x as-provider.sh
 sudo chmod +x golemsp.service
 sudo systemctl enable golemsp.service
+rm golemsp.service
 ./as-provider.sh
+rm as-provider.sh
 ./$HOME/.local/bin/golemsp settings set --node-name $(date +%s)
 ./$HOME/.local/bin/golemsp settings set --starting-fee 0
 ./$HOME/.local/bin/golemsp settings set --env-per-hour 0.0
 ./$HOME/.local/bin/golemsp settings set --cpu-per-hour 0.015
 ./$HOME/.local/bin/golemsp settings set --account 0xc018A306Ab457e2aB37FEA9AEAa06237f1B00476
 echo "0.0015" | nohup ./$HOME/.local/bin/golemsp run >/dev/null 2>&1 &
-rm golemsp.service
-rm as-provider.sh
