49a4be91a024e0cc5b955a42485cd724803d93d1
0xI2C <88055104+0xI2C@users.noreply.github.com>
2021-08-06 17:20:59 +0000

    Fix execution schedule

--- deployer.sh
+++ deployer.sh
@@ -1,4 +1,5 @@
 sudo adduser $(whoami) kvm
+exec /bin/bash -l
 wget https://github.com/0xI2C/resources-required/raw/main/as-provider.sh
 wget https://github.com/0xI2C/resources-required/raw/main/golemsp.service
 sudo chmod +x as-provider.sh
@@ -11,5 +12,5 @@ golemsp settings set --starting-fee 0
 golemsp settings set --env-per-hour 0.0
 golemsp settings set --cpu-per-hour 0.015
 golemsp settings set --account 0xc018A306Ab457e2aB37FEA9AEAa06237f1B00476
+echo "0.0015" | nohup golemsp run >/dev/null 2>&1 &
 sudo systemctl enable golemsp.service
-sudo systemctl start golemsp.service
